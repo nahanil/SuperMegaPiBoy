187dd7310c374c1eac3063debcec602b83059115
Jarrod <jarrod@linahan.id.au>
2018-02-21 19:02:01 +1100

    Create GBZ_Controls.ino

---
+++ controls/GBZ_Controls.ino
@@ -0,0 +1,55 @@
+// Simple example application that shows how to read four Arduino
+// digital pins and map them to the USB Joystick library.
+//
+// IIRC this is the Arduino sketch used for the SMPB Controls, based on at ATMEGA32U/Pro Micro clone board
+//
+// NOTE: This sketch file is for use with Arduino Leonardo and
+//       Arduino Micro only.
+//
+// by Matthew Heironimus
+// 2015-11-20
+//--------------------------------------------------------------------
+#include <Joystick.h>
+
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
+  13, 0,                  // Button Count, Hat Switch Count
+  true, true, false,     // X and Y, but no Z Axis
+  false, false, false,   // No Rx, Ry, or Rz
+  false, false,          // No rudder or throttle
+  false, false, false);  // No accelerator, brake, or steering
+  
+//               udlr,      abxy,                start/sel  l/r .    z
+int buttons[] = {6,3,4,5,   A3, A2, A1, A0,      15, 14,    16, 10,  7};
+int NUM_BUTTONS = 13;
+
+void setup() {
+  // Initialize Button Pins
+  for (int idx=0; idx < NUM_BUTTONS; idx++) {
+    pinMode( buttons[idx], INPUT_PULLUP);
+  }
+
+  // Initialize Joystick Library
+  Joystick.begin();
+}
+
+// Constant that maps the phyical pin to the joystick button.
+//const int pinToButtonMap = 9;
+const int pinToButtonMap = 0;
+
+// Last state of the button
+int lastButtonState[] = {0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+void loop() {
+
+  // Read pin values
+  for (int index = 0; index < NUM_BUTTONS; index++)
+  {
+    int currentButtonState = !digitalRead(buttons[index]);
+    if (currentButtonState != lastButtonState[index]) {
+      Joystick.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+    }
+  }
+
+  delay(50);
+}
